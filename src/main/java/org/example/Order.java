108ea36bb64c0359e9ea9efbbbe1ed7b0ef1fda9
Ivan <vanya.korobov@yandex.ru>
2025-03-10 01:43:36 +0300

    firstCommit

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Order {
+    private int id;
+    private String productName;
+    private int quantity;
+    private double unitPrice;
+
+    public Order(int id, String productName, int quantity, double unitPrice) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return getUnitPrice()*quantity;
+    }
+}
